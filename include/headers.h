0d713a964849f2f2fef3ae4b990a18fd17ff01b2
Paulo Sousa <a89465@alunos.uminho.pt>
2020-03-05 18:43:40 +0000

    Menu

---
+++ include/headers.h
@@ -0,0 +1,28 @@
+// Includes
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// Define
+
+#define MAX 40
+
+// Structs
+
+typedef enum {EMPTY, CURRENT, OCCUPIED} VALUE;
+
+typedef struct state
+{
+    VALUE table[8][8];
+} STATE;
+
+// Functions
+
+void input(STATE* e, char* name);
+
+void printa(STATE *e);
+void gamestart(STATE *e);
+
+void menu();
